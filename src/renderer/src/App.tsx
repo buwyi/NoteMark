fcd4520a49c30ab27f559c65c0c0606f0c12fbfa
buwyi <buwyi1104@hotmail.com>
2025-05-20 17:00:19 +0800

    Initialize the project including style

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,11 @@
 function App(): React.JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
-  return <></>
+  return (
+    <div className="h-full flex justify-center flex-items-center">
+      <div className="test m-0-auto hl-10"> today is wednesday</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import './assets/main.css';
+import '@unocss/reset/tailwind.css';
+import 'virtual:uno.css';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-)
+  </StrictMode>,
+);
